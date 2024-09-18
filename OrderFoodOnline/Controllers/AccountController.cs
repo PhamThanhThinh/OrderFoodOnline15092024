bb4d5ae7067768d0867660acbe324e05dbd459e7
thinh <thinhqn999@gmail.com>
2024-09-18 21:28:05 +0700

    final order food online

--- OrderFoodOnline/Controllers/AccountController.cs
+++ OrderFoodOnline/Controllers/AccountController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+using OrderFoodOnline.Models;
 
 namespace OrderFoodOnline.Controllers
 {
   public class AccountController : Controller
   {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+    }
+
     public IActionResult Login()
     {
       return View();
@@ -12,6 +24,58 @@ namespace OrderFoodOnline.Controllers
     public IActionResult Register()
     {
       return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel login)
+    {
+      if (ModelState.IsValid)
+      {
+        //var user = new ApplicationUser
+        //{
+        //  Email = login.Email
+        //};
+        //var result = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
+        var result = await _signInManager.PasswordSignInAsync(login.Email, login.Password, false, false);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+          ModelState.AddModelError("", "Không đăng nhập được. Xin vui lòng kiểm tra lại username và password");
+        }
+      }
+      return View(login);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel register)
+    {
+      if (ModelState.IsValid)
+      {
+        var user = new ApplicationUser
+        {
+          Name = register.Name,
+          Address = register.Address,
+          Email = register.Email,
+          UserName = register.Email
+        };
+        var result = await _userManager.CreateAsync(user, register.Password);
+        if (result.Succeeded)
+        {
+          await _signInManager.PasswordSignInAsync(user, register.Password, false, false);
+          return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+          foreach (var err in result.Errors)
+          {
+            ModelState.AddModelError("", err.Description);
+          }
+        }
+      }
+      return View(register);
     }
   }
 }
